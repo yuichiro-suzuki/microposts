906da05aaf4580493553bc226cfa91b9444bf9fc
yuichiro_suzuki <ninokashu@docomo.ne.jp>
2016-03-17 13:01:33 +0000

    removefinal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show 
    @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
+  end
   
   def new
     @user = User.new
