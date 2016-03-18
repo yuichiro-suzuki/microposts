f990321f1040c0c7ed165692f2d723fc9aa75b20
yuichiro_suzuki <ninokashu@docomo.ne.jp>
2016-03-18 14:38:33 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :collect_user, only: [:edit, :update] 
 
-  def show 
+  def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317053318) do
+ActiveRecord::Schema.define(version: 20160318141245) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
