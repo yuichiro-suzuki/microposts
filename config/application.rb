2d76728c6229b7b99282245672bf8584fe2db35e
yuichiro_suzuki <ninokashu@docomo.ne.jp>
2016-03-18 14:54:22 +0000

    add followings/followers

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
